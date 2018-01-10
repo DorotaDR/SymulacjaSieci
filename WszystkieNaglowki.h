405d0b106786645c54c9282b5056f2ce89489e02
DorotaDR <34707786+DorotaDR@users.noreply.github.com>
2018-01-10 09:14:05 +0100

    Update WszystkieNaglowki.h

--- WszystkieNaglowki.h
+++ WszystkieNaglowki.h
@@ -18,21 +18,14 @@ public:
 	std::vector<Worker> getWorkers();
 	std::vector<Storehause> getStorehouses();
 
-	void deleteRamps();
-	void deleteWorkers();
-	void deleteStorehouses();
+	void deleteRamps(std::vector<Ramp> rampsToDelete);
+	void deleteWorkers(std::vector<Worker> workersToDelete);
+	void deleteStorehouses(std::vector<Storehause> storehausesToDelete);
 }
 
 // ------ PRODUCT
 
 // ------ RAMP
-class Ramp{
-private: 
-	MathPreferences preferences;
-	double frequency;
-public: 
-	friend void SendTo();
-}
 
 // ------ WORKER
 
@@ -43,13 +36,50 @@ public:
 
 
 // ------ MATHPREFERENCES
+class MathPreferences{
+private:
+	std::map<int, int> preferences;
+	
+public: 
+	MathPreferences();
+	void add(int id, int prob);
+	void delete(int id);0
+}
+
 
 // ------ PRIORITY
+class Priority{\
+public:
+	virtual void push(int PackageId)=0; 		// umieszczenie nowego elementu na szczycie stosu;
+    	virtual void pop() = 0; 			// zdjęcie istniejącego elementu ze szczytu stosu;
+    	virtual bool empty() = 0; 			// informacja czy stos jest pusty;
+   	virtual int  top() = 0;				// zwraca wartość szczytowego elementu na stosie.
+}
+
+
 
 // ------ LIFO
+class Lifo: public Priority{
+private:
+	std::stack lifo;
+public:
+	void push(int PackageId); 	// umieszczenie nowego elementu na szczycie stosu;
+    	void pop(); 			// zdjęcie istniejącego elementu ze szczytu stosu;
+    	bool empty(); 			// informacja czy stos jest pusty;
+   	int  top();			// zwraca wartość szczytowego elementu na stosie.
+}
+
 
 // ------ FIFO
-
+class Fifo: public Priority{
+private:
+	std::stack fifo;
+public:
+	void push(int PackageId); 	// umieszczenie nowego elementu na szczycie stosu;
+    	void pop(); 			// zdjęcie istniejącego elementu ze szczytu stosu;
+    	bool empty(); 			// informacja czy stos jest pusty;
+   	int  top();			// zwraca wartość szczytowego elementu na stosie.
+}
 
 
 // ------ RAPORT
